6e62cb17d34375cbb402fd512ed9f16e5cee62e3
developersayem <xxxsayemmolla@gmail.com>
2023-06-07 18:44:59 +0600

    [Done]Setup Eslint,prettier,husky,lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-app.use(cors());
+app.use(cors())
 //PARSER
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app

8eb1491dc7c633b0a6ce4ce95f7f1c7eec0e1347
developersayem <xxxsayemmolla@gmail.com>
2023-06-14 10:12:56 +0600

    [Done]Setup Error handeler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application, NextFunction, Request, Response} from 'express'
+import express, { Application} from 'express'
 import cors from 'cors'
-import usersRouter from './app/modules/users/users.route'
-import { error } from 'winston'
+import globalErrorHandler from './app/middlewares/globalErrorHandeler'
+import { UserRoutes } from './app/modules/users/user.route'
 const app: Application = express()
 
 app.use(cors())
@@ -12,36 +12,14 @@ app.use(express.urlencoded({ extended: true }))
 
 // Application routes
 
-app.use('/api/v1/users/', usersRouter)
-
-//custom error handler
-class ApiError extends Error {
-  statusCode: number
-
-  constructor(statusCode: number, message: string | undefined, stack = '') {
-    super(message)
-    this.statusCode = statusCode
-    if (stack) {
-      this.stack = stack
-    } else {
-      Error.captureStackTrace(this, this.constructor)
-    }
-  }
-}
+app.use('/api/v1/users/', UserRoutes)
 
 // Testing
-app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  // next(new ApiError(400, 'Testing Error logger'))
-  next("error hoise")
-})
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+
+// })
 
 //global error handler
-app.use(err,req: Request, res: Response, next: NextFunction)=> {
-  if (err instanceof Error) {
-    res.status(400).json({error:err})
-  } else {
-    res.status(500).json({error:"something went wrong"})
-   }
-}
+app.use(globalErrorHandler)
 
 export default app
